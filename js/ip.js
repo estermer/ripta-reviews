023e5a2651b8e2a471e7c05f3e343594150c350b
estermer <eric.stermer@gmail.com>
2016-12-03 13:50:32 -0500

    added a review page and included a link to send the user to a google form to fill out

---
+++ js/ip.js
@@ -0,0 +1,1 @@
+var geo = navigator.geolocation;
---
+++ js/reviews.js
@@ -0,0 +1,11 @@
+(function(){
+  var app = angular.module('ripta-reviews', []);
+
+  app.controller('ReviewsCtrl', function($scope, $http){
+    $scope.writeReview = false;
+    $scope.readReviews = false;
+
+
+
+  });
+})();
