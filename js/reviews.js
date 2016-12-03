bb7fb08f59c012f5970ca346a4e4a73bc269b536
estermer <eric.stermer@gmail.com>
2016-12-03 14:29:33 -0500

    added angular and buttons to show reviews, added http request to get reviews to show the user

--- js/reviews.js
+++ js/reviews.js
@@ -1,11 +1,20 @@
 (function(){
   var app = angular.module('ripta-reviews', []);
-
+  console.log(app);
   app.controller('ReviewsCtrl', function($scope, $http){
-    $scope.writeReview = false;
     $scope.readReviews = false;
 
+    $scope.goToReviewsList = function(){
+      $scope.readReviews = true;
 
+      $http.get('/reviews')
+        .then(function(res){
+          $scope.reviewsList = res.data.reviews;
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+    };
 
   });
 })();
