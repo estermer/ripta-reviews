331748de0aff5e41676f8a818b239d91f74f01f0
Danny Kirschner <rkirschner1377@gmail.com>
2016-12-04 14:29:08 -0500

    Final touches: Foundation and images

--- js/gmaps.js
+++ js/gmaps.js
@@ -10,5 +10,26 @@ function initMap(lat, lng) {//pass a long and lat as parameters
     position: pvd,//pace the marker on this position
     map: map //place it on this map instance
   });
+
+var stop1 = {lat: 41.837486, lng: -71.423175};
+  var stop2 = {lat: 41.837280, lng: -71.421865};
+  var stop3 = {lat: 41.837063, lng: -71.421490}; //coordinates where we want the marker
+
+var marker = new google.maps.Marker({
+    position: stop1,//pace the marker on this position
+    icon: "img/bluepin.png",
+    map: map //place it on this map instance
+  });
+  var marker = new google.maps.Marker({
+    position: stop2,//pace the marker on this position
+    icon: "img/bluepin.png",    
+    map: map //place it on this map instance
+  });
+  var marker = new google.maps.Marker({
+    position: stop3,//pace the marker on this position
+    icon: "img/bluepin.png",
+    map: map //place it on this map instance
+  });
+
 }
 ///Creating a map from google maps///
